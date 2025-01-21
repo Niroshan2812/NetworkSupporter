9210ac4034da2e4b98f01c82f933a1f18826123e
Niroshan <niroshan9812@gmail.com>
2025-01-21 13:37:05 +0530

    initial commit

---
+++ src/main/java/org/niroshan/networksupporter/NetworkSupporterApplication.java
@@ -0,0 +1,13 @@
+package org.niroshan.networksupporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkSupporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkSupporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/niroshan/networksupporter/NetworkSupporterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.niroshan.networksupporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkSupporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
